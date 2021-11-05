511822b671a45d2737383a1fcc6af38d8da3a318
JimmerFish <jrticati@ncsu.edu>
2021-11-04 20:04:40 -0400

    Add info sections

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,35 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Bullshit words and stuff',
+    /* below all for button */
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/svg-3.svg').default,
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
+
+export const homeObjTwo = {
+    id: 'inventory',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Bullshit words and stuff',
+    /* below all for button */
+    buttonLabel: 'Get Started',
+    imgStart: true,
+    img: require('../../images/testimage.svg').default,
+    alt: 'Car',
+    dark: false,
+    primary: false,
+    darkText: true
+}
